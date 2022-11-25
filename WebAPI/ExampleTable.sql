8aa7afe3b13562aff37d1543e443a8c4df32ba0d
Alien663 <rite2844@gmail.com>
2022-11-25 11:41:03 +0800

    add create table script

---
+++ WebAPI/ExampleTable.sql
@@ -0,0 +1,18 @@
+﻿create table TT
+(
+	TID int identity(1, 1),
+	TName nvarchar(4),
+	TDes varchar(16),
+	constraint PK_TT primary key(TID)
+)
+go
+
+insert into TT(TName, TDes)
+	values
+		('我不會飛', 'abcd'),
+		('abcd', 'abcd'),
+		('A1', 'B1'),
+		('A2', 'B2'),
+		('A3', 'B3'),
+		('A4', 'B4')
+go
